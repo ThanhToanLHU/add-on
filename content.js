d80ba9cc301ec0ce09a9297501dfabb7f733b67b
tonghoangvu <tonghoangvu@outlook.com>
2021-03-20 18:34:01 +0700

    Download image on match

--- content.js
+++ content.js
@@ -1,2 +1,24 @@
-const url = window.location.href;
-console.log("URL: " + url);
+console.log(window.location.href);
+
+function isImagePath(path) {
+    // /PROFILE_WS/symlink/ ... Id_OK.jpg
+    return path &&
+        path.endsWith('.jpg');
+    return path &&
+        path.startsWith('/PROFILE_WS/symlink/') &&
+        path.endsWith('_OK.jpg');
+}
+
+if (isImagePath(window.location.pathname)) {
+    console.log('Can be downloaded');
+
+    const IMAGE_URL = window.location.href;
+    const FILE_NAME = IMAGE_URL.substring(IMAGE_URL.lastIndexOf('/') + 1);
+
+    const downloadLink = document.createElement('a');
+    downloadLink.setAttribute('href', IMAGE_URL);
+    downloadLink.setAttribute('download', FILE_NAME);
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+}
